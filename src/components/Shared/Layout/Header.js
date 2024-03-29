74ed87a480e62aba012f17e1b5d0421cbf962fb2
AimanSh123 <98344486+AimanSh123@users.noreply.github.com>
2024-03-29 06:04:29 +0530

    Update Header.js

--- src/components/Shared/Layout/Header.js
+++ src/components/Shared/Layout/Header.js
@@ -22,7 +22,9 @@ const Header = () => {
                     <li className ="nav-item mx-3">
                         <p className="nav-link">
                             <BiUserCircle/>
-                        Welcome {user.name} !
+                        Welcome {user?.name || user?.hospitalName || user?.organisationName}{" "}
+                          &nbsp; 
+                          <span className = "badge bg-secondary">{user?.role}</span>
                         </p></li>
                         <li className ="nav-item mx-3 ">
                            <button className ="btn btn-danger" onClick={handleLogout}>Logout</button>
